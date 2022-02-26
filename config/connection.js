46206961eb36c3a31b45900fa87d23522dd392d6
bvsswave <tannershahan@gmail.com>
2022-02-25 20:04:52 -0700

    Updated connection.js

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+const mysql = require("mysql");
+const util = require("util");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    // Your port; if not 3306
+    port: 3306,
+    // Your username
+    user: "root",
+    // Your password
+    password: "password",
+    database: "employee_db"
+});
+
+connection.connect();
+
+// Setting up connection.query to use promises instead of callbacks
+// This allows us to use the async/await syntax
+connection.query = util.promisify(connection.query);
+
+module.exports = connection;
